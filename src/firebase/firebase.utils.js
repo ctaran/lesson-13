7d3fd94f7cdd5181b8041635b9165827709cf214
Nicu Cristian Taran <cristian.taran.work@gmail.com>
2020-11-26 12:32:16 +0200

    cart item

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDmV_tfVZKAPYjzC5UqxFH0it7-Ukl6wO0",
+  authDomain: "crwn-db-659f9.firebaseapp.com",
+  databaseURL: "https://crwn-db-659f9.firebaseio.com",
+  projectId: "crwn-db-659f9",
+  storageBucket: "crwn-db-659f9.appspot.com",
+  messagingSenderId: "175225239930",
+  appId: "1:175225239930:web:cf9b98510278b3e9a83ec3"
 };
 
 firebase.initializeApp(config);
